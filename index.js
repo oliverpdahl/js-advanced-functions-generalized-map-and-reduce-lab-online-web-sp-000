fd5d417695a2bc3c3138fbe9a70f14233e51e6cf
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-12 15:07:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(array, action){
+  let newArray = []
+  for(const num of array){
+    newArray.push(action(num))
+  }
+  return newArray
+}
+
+function reduce(array, action, startingPoint = false){
+  let value
+  let pointer
+  let newArray = [...array]
+  if(startingPoint){
+    value = startingPoint
+    pointer = 0
+  } else{
+    value = newArray.shift()
+    pointer = 1
+  }
+  for(pointer; pointer < array.length; pointer++){
+    value = action(value, array[pointer])
+  }
+  return value
+}
